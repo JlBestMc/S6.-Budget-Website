30cf35b67a893b6e73a77a6a96b1038da258ce6f
JlBestMc <jlrcmt@gmail.com>
2025-07-05 15:55:26 +0200

    Abstracted business logic "calculateTotal" from the component to a separate file, inside the "lib" directory. Also, updated the hook "useSelectableCard" to use this new function. Finally, CalculatorPage now imports the new function instead of the old one.

--- src/features/cards/hooks/useSelectableCard.tsx
+++ src/features/cards/hooks/useSelectableCard.tsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 
 export function useSelectableServices() {
   const [selected, setSelected] = useState<number[]>([]);
+  const [numPages, setNumPages] = useState(1);
+  const [numLanguages, setNumLanguages] = useState(1);
 
   const toggleService = (id: number) => {
     setSelected((prev) =>
@@ -9,5 +11,12 @@ export function useSelectableServices() {
     );
   };
 
-  return { selected, toggleService };
+  return {
+    selected,
+    toggleService,
+    numPages,
+    numLanguages,
+    setNumPages,
+    setNumLanguages,
+  };
 }
--- src/pages/CalculatorPage.tsx
+++ src/pages/CalculatorPage.tsx
@@ -1,15 +1,24 @@
 import ServiceCards from "../features/cards/components/ServiceCards";
-import { useSelectableServices } from "../features/cards/hooks/useSelectableCard";
 import { SERVICES } from "../features/cards/data/services";
 import bgCard from "../assets/bg-card.jpg";
+import { calculateTotal } from "../features/cards/lib/calculateTotal";
+import { useSelectableServices } from "../features/cards/hooks/useSelectableCard";
 
 export default function CalculatorPage() {
-  const { selected, toggleService } = useSelectableServices();
 
-  const total = selected.reduce((sum, id) => {
-    const service = SERVICES.find((s) => s.id === id);
-    return sum + (service ? service.price : 0);
-  }, 0);
+  const {
+    selected,
+    toggleService,
+    numPages,
+    numLanguages,
+    setNumPages,
+    setNumLanguages,
+  } = useSelectableServices();
+
+   const total = calculateTotal(selected, SERVICES, {
+    pages: numPages,
+    languages: numLanguages,
+  });
 
   return (
     <>
@@ -22,11 +31,19 @@ export default function CalculatorPage() {
         </h1>
       </div>
       <div className="max-w-3xl mx-auto p-6 space-y-6">
-        <ServiceCards selected={selected} onToggle={toggleService} />
+        <ServiceCards selected={selected}
+        onToggle={toggleService}
+        webOptions={{
+          numPages,
+          numLanguages,
+          onChangePages: setNumPages,
+          onChangeLanguages: setNumLanguages,
+        }} />
         <div className="text-right font-bold text-xl">
-          Preu pressupostat: {total} €
+          Preu pressupostat: {total.toFixed(2)} €
         </div>
       </div>
     </>
   );
 }
+
